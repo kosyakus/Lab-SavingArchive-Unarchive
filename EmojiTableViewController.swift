4a7a8e229c50c39ee26126726beb082795f9cc98
Unknown <kosyak_@mail.ru>
2017-09-25 22:18:05 +0300

    Created update func - to abstract the setup code from tableView cellForRowAt method to cell itself

--- EmojiTableViewController.swift
+++ EmojiTableViewController.swift
@@ -55,13 +55,15 @@ class EmojiTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath) as! EmojiTableViewCell
 
         let emoji = emojis[indexPath.row]
+                
+        //cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+        //cell.detailTextLabel?.text = emoji.description
+
+        cell.update(with: emoji)
         cell.showsReorderControl = true // reorder cells
-        
-        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
-        cell.detailTextLabel?.text = emoji.description
 
         return cell
     }
