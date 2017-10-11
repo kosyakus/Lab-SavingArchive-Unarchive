4c810ac02a6542f6227b6f9149268050fe9dd246
Unknown <kosyak_@mail.ru>
2017-10-11 14:48:18 +0300

    pass data to static TVC cells

--- Emoji-AppDevWSwift/AddEditEmojiTableViewController.swift
+++ Emoji-AppDevWSwift/AddEditEmojiTableViewController.swift
@@ -12,13 +12,21 @@ class AddEditEmojiTableViewController: UITableViewController {
     
     var emoji: Emoji?
 
+    @IBOutlet weak var symbolTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var usageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // check if emoji has a value. if it does, then edit existing emoji
+        if let emoji = emoji {
+            symbolTextField.text = emoji.symbol
+            nameTextField.text = emoji.name
+            descriptionTextField.text = emoji.description
+            usageTextField.text = emoji.usage
+        }
+        
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
